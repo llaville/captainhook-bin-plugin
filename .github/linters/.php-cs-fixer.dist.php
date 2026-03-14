888570b437b6e58dc21cdbfde1f112a962b352a6
Laurent Laville <laurent.laville@gmail.com>
2026-03-14 06:12:44 +0000

    introduce the PHP-CS-Fixer (dist) config file

---
+++ .github/linters/.php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in('.')
+;
+
+return (new PhpCsFixer\Config())
+    // allow to run on unsupported PHP Versions
+    // {@link https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/README.md#supported-php-versions}
+    ->setUnsupportedPhpVersionAllowed(true)
+    // use default @link https://www.php-fig.org/per/coding-style/
+    ->setRules([
+        '@PER-CS' => true,
+        'single_line_empty_body' => false,
+    ])
+    // default source code to scan
+    ->setFinder($finder)
+;
